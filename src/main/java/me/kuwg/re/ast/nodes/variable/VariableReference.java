2458593ba4341aa08ece2dbc736f906ce3c400dd
Kuwg <endahpvp@gmail.com>
2025-12-15 14:50:47 +0100

    Fixed Variable Reference useless implementations

--- src/main/java/me/kuwg/re/ast/nodes/variable/VariableReference.java
+++ src/main/java/me/kuwg/re/ast/nodes/variable/VariableReference.java
@@ -1,12 +1,10 @@
 package me.kuwg.re.ast.nodes.variable;
 
 import me.kuwg.re.ast.value.ValueNode;
-import me.kuwg.re.compiler.Compilable;
 import me.kuwg.re.compiler.CompilationContext;
 import me.kuwg.re.compiler.variable.RVariable;
-import me.kuwg.re.writer.Writeable;
 
-public abstract class VariableReference extends ValueNode implements Compilable, Writeable {
+public abstract class VariableReference extends ValueNode {
     protected VariableReference(final int line) {
         super(line);
     }
