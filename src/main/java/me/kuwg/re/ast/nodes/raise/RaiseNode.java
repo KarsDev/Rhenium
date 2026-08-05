e45c5e8cc4939f41928579d68be85a60fcc84823
Kuwg <endahpvp@gmail.com>
2026-08-05 11:59:41 +0200

    Added error raising support

--- src/main/java/me/kuwg/re/ast/nodes/raise/RaiseNode.java
+++ src/main/java/me/kuwg/re/ast/nodes/raise/RaiseNode.java
@@ -4,14 +4,18 @@ import me.kuwg.re.ast.ASTNode;
 import me.kuwg.re.ast.nodes.constants.NumberNode;
 import me.kuwg.re.ast.nodes.constants.StringNode;
 import me.kuwg.re.ast.nodes.function.call.FunctionCallNode;
+import me.kuwg.re.ast.nodes.function.call.StructFunctionCallNode;
 import me.kuwg.re.ast.types.interrupt.InterruptNode;
 import me.kuwg.re.ast.types.value.ValueNode;
 import me.kuwg.re.compiler.CompilationContext;
+import me.kuwg.re.compiler.struct.RDefaultStruct;
 import me.kuwg.re.error.errors.RInternalError;
 import me.kuwg.re.error.errors.variable.RVariableTypeError;
 import me.kuwg.re.type.TypeRef;
 import me.kuwg.re.type.builtin.BuiltinTypes;
+import me.kuwg.re.type.struct.StructType;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -43,54 +47,28 @@ public class RaiseNode extends ASTNode implements InterruptNode {
                         List.of(new StringNode(fileName, line, message))
                 ).compile(cctx);
             } else {
-                String valueReg = value.compileAndGet(cctx);
+                ValueNode cloned = value.clone();
+                String valueReg = cloned.compileAndGet(cctx);
+                TypeRef type = cloned.getType();
 
-                if (!(value.getType() == BuiltinTypes.STR.getType())) {
-                    new RVariableTypeError("str", value.getType().getName(), fileName, line).raise();
+                LABEL_O1: {
+                    if (type == BuiltinTypes.STR.getType()) {
+                        compileString(valueReg, cctx);
+                        break LABEL_O1;
+                    } else if (type instanceof StructType st) {
+                        RDefaultStruct struct = cctx.getStruct(st.getName());
+                        if (struct != null && struct.inherited().contains("Error")) {
+                            compileError(cctx);
+                            break LABEL_O1;
+                        }
+                    }
+
+                    new RVariableTypeError("Raise only supports Error or string types: " + type.getName() + " is not supported", fileName, line).raise();
                     return;
                 }
 
-                String message = generateLog(line, cctx.writeExceptionLines);
 
-                new FunctionCallNode(
-                        fileName, line,
-                        "println",
-                        List.of(new ValueNode(fileName, line, BuiltinTypes.STR.getType()) {
 
-                            @Override
-                            public void write(final StringBuilder sb, final String indent) {
-                                sb.append(indent).append("Error Value").append(NEWLINE);
-                            }
-
-                            @Override
-                            public void replaceGenerics(final Map<String, TypeRef> generics, final CompilationContext cctx) {
-                            }
-
-                            @Override
-                            public void compile(final CompilationContext cctx) {
-                                throw new RInternalError("Should be compiled via compileAndGet");
-                            }
-
-                            @Override
-                            public String compileAndGet(final CompilationContext cctx) {
-                                String strReg = new StringNode(fileName, line, message).compileAndGet(cctx);
-                                String msgReg = cctx.nextRegister();
-
-                                cctx.emit(
-                                        msgReg + " = call i8* @strConcat(i8* " +
-                                                strReg +
-                                                ", i8* " + valueReg + ")"
-                                );
-
-                                return msgReg;
-                            }
-
-                            @Override
-                            public ValueNode clone() {
-                                return this;
-                            }
-                        })
-                ).compile(cctx);
             }
 
             new FunctionCallNode(
@@ -103,6 +81,107 @@ public class RaiseNode extends ASTNode implements InterruptNode {
         } else {
             cctx.emit("br label %" + catchLabel);
         }
+    }
+
+    private void compileString(String valueReg, CompilationContext cctx) {
+        String message = generateLog(line, cctx.writeExceptionLines);
+
+        new FunctionCallNode(
+                fileName, line,
+                "println",
+                List.of(new ValueNode(fileName, line, BuiltinTypes.STR.getType()) {
+
+                    @Override
+                    public void write(final StringBuilder sb, final String indent) {
+                        sb.append(indent).append("Error Value").append(NEWLINE);
+                    }
+
+                    @Override
+                    public void replaceGenerics(final Map<String, TypeRef> generics, final CompilationContext cctx) {
+                    }
+
+                    @Override
+                    public void compile(final CompilationContext cctx) {
+                        throw new RInternalError("Should be compiled via compileAndGet");
+                    }
+
+                    @Override
+                    public String compileAndGet(final CompilationContext cctx) {
+                        String strReg = new StringNode(fileName, line, message).compileAndGet(cctx);
+                        String msgReg = cctx.nextRegister();
+
+                        cctx.emit(
+                                msgReg + " = call i8* @strConcat(i8* " +
+                                        strReg +
+                                        ", i8* " + valueReg + ")"
+                        );
+
+                        return msgReg;
+                    }
+
+                    @Override
+                    public ValueNode clone() {
+                        return this;
+                    }
+                })
+        ).compile(cctx);
+    }
+
+    private void compileError(CompilationContext cctx) {
+        StructFunctionCallNode messageCall =
+                new StructFunctionCallNode(fileName, line, value, "message", new ArrayList<>());
+
+        String messageReg = messageCall.compileAndGet(cctx);
+
+        String errorName = value.getType().getName();
+
+        String nameReg = new StringNode(
+                fileName,
+                line,
+                errorName + ": "
+        ).compileAndGet(cctx);
+
+        String fullMessageReg = cctx.nextRegister();
+
+        cctx.emit(
+                fullMessageReg + " = call i8* @strConcat(i8* "
+                        + nameReg
+                        + ", i8* "
+                        + messageReg
+                        + ")"
+        );
+
+        new FunctionCallNode(
+                fileName,
+                line,
+                "println",
+                List.of(new ValueNode(fileName, line, BuiltinTypes.STR.getType()) {
+
+                    @Override
+                    public void write(StringBuilder sb, String indent) {
+                        sb.append(indent).append("Error").append(NEWLINE);
+                    }
+
+                    @Override
+                    public void replaceGenerics(Map<String, TypeRef> generics, CompilationContext cctx) {
+                    }
+
+                    @Override
+                    public void compile(CompilationContext cctx) {
+                        throw new RInternalError("Should be compiled via compileAndGet");
+                    }
+
+                    @Override
+                    public String compileAndGet(CompilationContext cctx) {
+                        return fullMessageReg;
+                    }
+
+                    @Override
+                    public ValueNode clone() {
+                        return this;
+                    }
+                })
+        ).compile(cctx);
     }
 
     @Override
